bd18ed65ca09e058ed367f3515303a1af1a98382
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-08-01 17:06:47 -0400

    Adding article and comment schema

---
+++ models/Comment.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// create comment schema
+var CommentSchema = new Schema({
+  title: String,
+  body: String
+});
+
+// create model from Schema
+var Comment = mongoose.model("Comment", CommentSchema);
+
+// export the Comment model
+module.exports = Comment;
